14f2e9a21c03177cf75c19c2fff1bad4a199a4da
Okan Koca <oknakoca@gmail.com>
2024-10-10 14:20:24 +0200

    added second player and controls, disabled caching for nginx for development

--- srcs/frontend/js/game.js
+++ srcs/frontend/js/game.js
@@ -12,26 +12,35 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(0.3, 2, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-
-const light = new THREE.AmbientLight(0x404040); // soft white light
+const light = new THREE.PointLight(0xff0000, 1, 100);
+light.position.set(5, 5, 5);
 scene.add(light);
-scene.add(cube);
+
 camera.position.z = 5;
 
-const screen_to_world = (x, y) => {
-	const vec = new THREE.Vector3(x * 10, y * 10, 0.5);
-	vec.unproject(camera);
-	return vec;
+const addCube = (scene, side, color) => {
+	color = color || 0x00ff00;
+	const geometry = new THREE.BoxGeometry(0.3, 2, 1);
+	const material = new THREE.MeshBasicMaterial({ color });
+	const cube = new THREE.Mesh(geometry, material);
+	scene.add(cube);
+
+	const maxY = 5 - cube.scale.y * 3;
+	if (side == "left") cube.position.set(-5 - cube.scale.x + 0.1, maxY, 0);
+	else cube.position.set(5 + cube.scale.x - 0.1, maxY, 0);
+
+	return cube;
 };
 
-const top_left = screen_to_world(-1.05, 0.8);
+const leftCube = addCube(scene, "left");
+const rightCube = addCube(scene, "right", 0xffaabb);
 
-cube.position.set(top_left.x, top_left.y, 0);
+const leftMovement = {
+	up: false,
+	down: false,
+};
 
-const movement = {
+const rightMovement = {
 	up: false,
 	down: false,
 };
@@ -39,12 +48,16 @@ const movement = {
 document.addEventListener("keydown", (event) => {
 	switch (event.code) {
 		case "KeyW":
+			leftMovement.up = true;
+			break;
 		case "ArrowUp":
-			movement.up = true;
+			rightMovement.up = true;
 			break;
 		case "KeyS":
+			leftMovement.down = true;
+			break;
 		case "ArrowDown":
-			movement.down = true;
+			rightMovement.down = true;
 			break;
 	}
 });
@@ -52,23 +65,45 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keyup", (event) => {
 	switch (event.code) {
 		case "KeyW":
+			leftMovement.up = false;
+			break;
 		case "ArrowUp":
-			movement.up = false;
+			rightMovement.up = false;
 			break;
 		case "KeyS":
+			leftMovement.down = false;
+			break;
 		case "ArrowDown":
-			movement.down = false;
+			rightMovement.down = false;
 			break;
 	}
 });
 
-const animate = () => {
+const getBoundaryMin = (cube) => {
+	return -5 + cube.scale.y * 2;
+};
+
+const getBoundaryMax = (cube) => {
+	return 5 - cube.scale.y * 2;
+};
+
+const handleMovement = (movement, cube, min, max) => {
 	if (movement.up) {
 		cube.translateY(0.05);
 	}
 	if (movement.down) {
 		cube.translateY(-0.05);
 	}
+	cube.position.y = THREE.MathUtils.clamp(
+		cube.position.y,
+		getBoundaryMin(cube),
+		getBoundaryMax(cube)
+	);
+};
+
+const animate = () => {
+	handleMovement(leftMovement, leftCube);
+	handleMovement(rightMovement, rightCube);
 	renderer.render(scene, camera);
 };
 
