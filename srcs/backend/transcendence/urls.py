9e9e8506c78f0347bce62be60baff4d84e2b7ccf
Okan Koca <oknakoca@gmail.com>
2024-10-11 08:22:20 +0200

    Merge backend branch and fixed issue with project name

---
+++ srcs/backend/transcendence/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for transcendence project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from authentication.views import (
+    RegisterView,
+    LoginView,
+    RefreshTokenView,
+    LogoutView,
+    OAuth42LoginView,
+    OAuth42CallbackView,
+)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("o/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("api/auth/register/", RegisterView.as_view(), name="register"),
+    path("api/auth/login/", LoginView.as_view(), name="login"),
+    path("api/auth/token/refresh/", RefreshTokenView.as_view(), name="token_refresh"),
+    path("api/auth/logout/", LogoutView.as_view(), name="logout"),
+    path("api/oauth/42/", OAuth42LoginView.as_view(), name="oauth_42_login"),
+    path(
+        "api/oauth/42/callback/",
+        OAuth42CallbackView.as_view(),
+        name="oauth_42_callback",
+    ),
+]
